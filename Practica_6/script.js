bc2228e6ee3f199a8b8ee02fcf6ed537ba7cb616
YABRYX <ortegabryan7872@gmail.com>
2026-06-09 10:40:32 -0400

    Update script.js

--- Practica_6/script.js
+++ Practica_6/script.js
@@ -1,7 +1,5 @@
 const pantalla = document.getElementById('pantalla');
 let expresion = '';
-
-// Agregar valor a la expresión
 document.querySelectorAll('[data-valor]').forEach(boton => {
     boton.addEventListener('click', () => {
         expresion += boton.dataset.valor;
@@ -9,11 +7,9 @@ document.querySelectorAll('[data-valor]').forEach(boton => {
     });
 });
 
-// Funciones de control
 document.querySelectorAll('[data-comando]').forEach(boton => {
     boton.addEventListener('click', () => {
         const accion = boton.dataset.comando;
-        
         switch(accion) {
             case 'borrar':
                 expresion = expresion.slice(0, -1);
@@ -23,7 +19,6 @@ document.querySelectorAll('[data-comando]').forEach(boton => {
                 break;
             case 'calcular':
                 try {
-                    // Reemplazar símbolos visuales por operadores válidos
                     let calculo = expresion.replace('×', '*').replace('÷', '/');
                     const resultado = eval(calculo);
                     expresion = resultado.toString();
